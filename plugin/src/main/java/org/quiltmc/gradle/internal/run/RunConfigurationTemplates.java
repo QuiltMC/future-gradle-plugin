32ad4351e70333bfac8e16d6d35255cbf2446bcd
i509VCB <git@i509.me>
2021-05-12 13:49:55 -0500

    And the internal package

---
+++ plugin/src/main/java/org/quiltmc/gradle/internal/run/RunConfigurationTemplates.java
@@ -0,0 +1,25 @@
+package org.quiltmc.gradle.internal.run;
+
+import org.quiltmc.gradle.api.run.RunConfiguration;
+
+/**
+ * Templates used to generate the default Minecraft client and server run configurations.
+ */
+public final class RunConfigurationTemplates {
+	/**
+	 * Configures the default Minecraft client configuration.
+	 */
+	public static void client(RunConfiguration configuration) {
+		// TODO
+	}
+
+	/**
+	 * Configures the default Minecraft server configuration.
+	 */
+	public static void server(RunConfiguration configuration) {
+		// TODO
+	}
+
+	private RunConfigurationTemplates() {
+	}
+}
